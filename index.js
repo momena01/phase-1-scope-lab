aa551350659e10d7845f0050c1ac6b5800d3d32b
momena <momena.salloum59@gmail.com>
2023-10-09 00:42:57 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// helpers.js
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    // Attempting to reassign a constant will result in an error.
+    throw new Error('Assignment to constant variable.');
+}
